677cabd093b8219c03e315f27d19265321391f18
brenno luan <brennoluan8530@gmail.com>
2026-06-16 19:57:30 -0300

    feat: implement ModalComment component with form for user comments and associated styles

---
+++ src/components/ModalComment/index.jsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useRef } from 'react';
+import { IconButton } from '../IconButton';
+import { Chat } from '../icons/Chat';
+import { Modal } from '../Modal';
+import styles from './modalcomment.module.css';
+import { Subheading } from '../Subheading';
+import { Textarea } from '../Textarea';
+import { SubmitButton } from '../SubmitButton';
+
+export const ModalComment = ({ action }) => {
+  const modalRef = useRef(null);
+
+  return (
+    <>
+      <Modal ref={modalRef}>
+        <form action={action} onSubmit={() => modalRef.current.closeModal()}>
+          <Subheading>Deixe seu comentário sobre o post:</Subheading>
+          <Textarea
+            required
+            rows={8}
+            name="text"
+            placeholder="Digite aqui..."
+          />
+          <div className={styles.footer}>
+            <SubmitButton>Comentar</SubmitButton>
+          </div>
+        </form>
+      </Modal>
+      <IconButton onClick={() => modalRef.current.openModal()}>
+        <Chat />
+      </IconButton>
+    </>
+  );
+};
