18955af4f0ec86330775436b659f899c95b1e8db
brenno luan <brennoluan8530@gmail.com>
2026-06-16 19:57:52 -0300

    refactor: streamline getPostBySlug function and improve error handling

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,55 +1,54 @@
-import logger from "@/logger"
+import logger from '@/logger';
 import { remark } from 'remark';
 import html from 'remark-html';
 
-import styles from './page.module.css'
-import { CardPost } from "@/components/CardPost";
-import db from "../../../../prisma/db";
-import { redirect } from "next/navigation";
-
+import styles from './page.module.css';
+import { CardPost } from '@/components/CardPost';
+import db from '../../../../prisma/db';
+import { redirect } from 'next/navigation';
 
 async function getPostBySlug(slug) {
+  try {
+    const post = await db.post.findFirst({
+      where: {
+        slug,
+      },
+      include: {
+        author: true,
+        comments: true,
+      },
+    });
 
-    try {
-        const post = await db.post.findFirst({
-            where: { 
-                slug
-            },
-            include: {
-                author: true
-            }
-        })
-
-        if (!post) {
-            throw new Error(`Post com o slug ${slug} não foi encontrado`)
-        }
-    
-        const processedContent = await remark()
-            .use(html)
-            .process(post.markdown);
-        const contentHtml = processedContent.toString();
-    
-        post.markdown = contentHtml
-    
-        return post
-    } catch (error) {
-        logger.error('Falha ao obter o post com o slug: ', {
-            slug,
-            error
-        })   
+    if (!post) {
+      throw new Error(`Post com o slug ${slug} não foi encontrado`);
     }
-    redirect('/not-found')
+
+    const processedContent = await remark().use(html).process(post.markdown);
+    const contentHtml = processedContent.toString();
+
+    post.markdown = contentHtml;
+
+    return post;
+  } catch (error) {
+    logger.error('Falha ao obter o post com o slug: ', {
+      slug,
+      error,
+    });
+  }
+  redirect('/not-found');
 }
 
 const PagePost = async ({ params }) => {
-    const post = await getPostBySlug(params.slug)
-    return (<div>
-        <CardPost post={post} highlight />
-        <h3 className={styles.subtitle}>Código:</h3>
-        <div className={styles.code}>
-            <div dangerouslySetInnerHTML={{ __html: post.markdown }} />
-        </div>
-    </div>)
-}
+  const post = await getPostBySlug(params.slug);
+  return (
+    <div>
+      <CardPost post={post} highlight />
+      <h3 className={styles.subtitle}>Código:</h3>
+      <div className={styles.code}>
+        <div dangerouslySetInnerHTML={{ __html: post.markdown }} />
+      </div>
+    </div>
+  );
+};
 
-export default PagePost
+export default PagePost;
